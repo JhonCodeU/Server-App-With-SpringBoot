0888c4a91aeb465d2f1c1e9afb5fd152f89dd313
John Arcila Castano <jhon.380172793@ucaldas.edu.co>
2023-07-22 13:33:22 -0500

    structure of proyecto

---
+++ src/main/java/com/getarrays/Server/model/Server.java
@@ -0,0 +1,13 @@
+package com.getarrays.Server.model;
+
+import com.getarrays.Server.enumeration.Status;
+
+public class Server {
+    private Long id;
+    private String ipAddress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imageUrl;
+    private Status status;
+}
