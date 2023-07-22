7df59a3478208424b562661ca9a92d1db6c1395c
John Arcila Castano <jhon.380172793@ucaldas.edu.co>
2023-07-22 18:40:05 -0500

    create repository, service, implementation, model

---
+++ src/main/java/com/getarrays/Server/service/implementation/ServerServiceImplementation.java
@@ -0,0 +1,69 @@
+package com.getarrays.Server.service.implementation;
+
+import com.getarrays.Server.enumeration.Status;
+import com.getarrays.Server.model.Server;
+import com.getarrays.Server.repo.ServerRepository;
+import com.getarrays.Server.service.ServerService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Collection;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
+public class ServerServiceImplementation  implements ServerService {
+    private  final ServerRepository serverRepository;
+    @Override
+    public Server create(Server server) {
+        log.info("Creating server {}", server.getName());
+        server.setImageUrl(setServerImageUrl());
+        return serverRepository.save(server);
+    }
+
+    @Override
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging server IP: {}", ipAddress);
+        Server server = serverRepository.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(10000) ? Status.SERVER_UP : Status.SERVER_DOWN);
+        serverRepository.save(server);
+        return server;
+    }
+
+    @Override
+    public Collection<Server> list(int limit) {
+        log.info("Fetching all servers");
+        return serverRepository.findAll(PageRequest.of(0, limit)).toList();
+    }
+
+    @Override
+    public Server get(long id) {
+        log.info("Fetching server with id {}", id);
+        return serverRepository.findById(id).get();
+    }
+
+    @Override
+    public Server update(Server server) {
+        log.info("Updating server with id {}", server.getId());
+        return serverRepository.save(server);
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        log.info("Deleting server with id {}", id);
+        serverRepository.deleteById(id);
+        return Boolean.TRUE;
+    }
+
+    private String setServerImageUrl() {
+        return null;
+    }
+}
